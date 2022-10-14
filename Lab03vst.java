98e1eb92041100e2b66d7ffc74449269f0df95f9
Gabe <gabriel.lupino@vtsd.com>
2022-10-14 09:23:13 -0400

    Lab 03

--- Lab03vst.java
+++ Lab03vst.java
@@ -9,7 +9,17 @@ public class Lab03vst
 	public static void main(String[] args)
 	{
       System.out.println("Lab03, 80 Point Version\n");
-      
+	  int startSeconds = 10000;
+	  int hours = startSeconds/ 3600;
+	  int hRemainder = startSeconds % 3600;
+	  int mins = hRemainder / 60;
+	  int seconds = hRemainder % 60;
+	  System.out.println("Starting seconds    :" + startSeconds);
+	  System.out.println("Hours:   " + hours);
+	  System.out.println("Minutes:   " + mins);
+	  System.out.println("Seconds:   " + seconds);
+
+
 	}
 }
 
